21345889529691149709e32387206f36099011b5
bernardinoleite <bernardinoleite2008@gmail.com>
2025-02-23 20:16:34 +0100

    Add file upload functionality to new project form

--- js/newProject.js
+++ js/newProject.js
@@ -14,7 +14,6 @@ class NewProject {
 
             const formData = new FormData(e.target)
             const datas = Object.fromEntries(formData)
-
             for (let data in datas) {
 
                 if (!datas[data]) {
@@ -25,7 +24,20 @@ class NewProject {
             }
 
             const token = localStorage.getItem("token")
+            const fileForm = new FormData();
+            fileForm.append("file", e.target[7].files[0]); // Apenas adiciona ao FormData
 
+            const responseUrl = await fetch(config.routerProjectUpload, {
+                method: "POST",
+                body: fileForm, // Passa o FormData corretamente
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            });
+
+            const url = await responseUrl.json()
+            datas.file_path = url.url
+            console.log(datas)
             const request = await fetch(config.routerNewProject, {
                 method: "POST",
                 body: JSON.stringify(datas),
@@ -37,6 +49,7 @@ class NewProject {
 
 
             const response = await request.json()
+            console.log(response);
 
             if (response.id) {
                 alert("Projecto cadastrado com sucesso")
